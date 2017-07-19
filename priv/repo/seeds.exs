36ca8596ea7801a29795bf5f4e6e2aac2a3fd5b1
surajit-chongder <surajit.chongder@gmail.com>
2017-07-19 11:55:33 +0530

    [Surajit/Anusree] Modified Course view for showing professor and Course name

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,17 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias EnrollmentApp.Professor
+alias EnrollmentApp.Student
+alias EnrollmentApp.Course
+alias EnrollmentApp.Department
+alias EnrollmentApp.Repo
+
+Repo.insert!(%Department{name: "Physics"})
+
+Repo.insert!(%Professor{name: "Ram Yadav",department_id: Repo.get_by(Department, name: "Physics").id})
+
+#Repo.insert!(%Student{name: "Saram Ram",address: "Chennai",email_id: "something@gmail.com"})
+
+Repo.insert!(%Course{name: "Science", department_id: Repo.get_by(Department, name: "Physics").id, professor_id: Repo.get_by(Professor, name: "Ram Yadav").id})
--- web/views/course_view.ex
+++ web/views/course_view.ex
@@ -1,3 +1,16 @@
 defmodule EnrollmentApp.CourseView do
   use EnrollmentApp.Web, :view
+
+  alias EnrollmentApp.Department
+  alias EnrollmentApp.Professor
+  alias EnrollmentApp.Repo
+
+  def get_department_name(department_id) do
+    Repo.get!(Department, department_id).name
+  end
+
+  def get_professor_name(professor_id) do
+    Repo.get!(Professor, professor_id).name
+  end
+
 end
